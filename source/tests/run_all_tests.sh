ab1366c42934fda0dc20b614d42aff57810c4af0
is-nobody <winstonsalam@proton.me>
2026-06-06 17:58:00 +0700

    refactor: update run all tests file

---
+++ source/tests/run_all_tests.sh
@@ -0,0 +1,30 @@
+# ARITHMETIC
+apex source/tests/arithmetic/whole_with_whole.apex
+apex source/tests/arithmetic/decimal_with_decimal.apex
+apex source/tests/arithmetic/whole_with_decimal.apex
+apex source/tests/arithmetic/priority_without_parentheses.apex
+apex source/tests/arithmetic/priority_parentheses.apex
+
+# CONDITIONALS
+## IF-ELIF-ELSE
+apex source/tests/conditionals/if-elif-else/if_single.apex
+apex source/tests/conditionals/if-elif-else/if-elif-else.apex
+apex source/tests/conditionals/if-elif-else/if-else.apex
+apex source/tests/conditionals/if-elif-else/nested-if.apex
+## COMPARISONS
+apex source/tests/conditionals/comparisons/whole.apex
+apex source/tests/conditionals/comparisons/decimal.apex
+apex source/tests/conditionals/comparisons/string.apex
+apex source/tests/conditionals/comparisons/boolean.apex
+## LOGICAL
+apex source/tests/conditionals/logical/and.apex
+apex source/tests/conditionals/logical/not.apex
+apex source/tests/conditionals/logical/or.apex
+
+# LOOPS
+apex source/tests/loops/for.apex
+apex source/tests/loops/while.apex
+
+# TYPE CONVERSION
+apex source/tests/type_conversion/number.apex
+apex source/tests/type_conversion/string.apex
