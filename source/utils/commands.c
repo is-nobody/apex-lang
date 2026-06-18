baef407e42bf6b111b39b06edda1987621629c96
is-nobody <winstonsalam@proton.me>
2026-06-18 23:06:53 +0700

    refactor: delete vfs packer

--- source/utils/commands.c
+++ source/utils/commands.c
@@ -3,7 +3,6 @@
 #include "platform.h"
 #include "build.h"
 #include "package_manager.h"
-#include "bridge.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
@@ -41,11 +40,6 @@ int handle_commands(int argc, char** argv) {
     // --- BUILD COMMAND ---
     if (strcmp(argv[1], "build") == 0) {
         return build_command(argc, argv);
-    }
-
-    // --- SETTINGS COMMAND ---
-    if (strcmp(argv[1], "settings") == 0) {
-        return bridge_execute_settings() ? 0 : 1;
     }
 
     // --- INSTALL COMMAND ---
