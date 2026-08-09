0eb5dae407a16692b73f10d54be3bd4e1ad75754
is-nobody <winstonsalam@proton.me>
2026-08-09 18:39:31 +0700

    refactor: add documentation comments to test suite

--- source/tests/libraries/libtest.c
+++ source/tests/libraries/libtest.c
@@ -1,3 +1,8 @@
+// source/tests/libraries/libtest.c
+// Implementation of Test File for FFI Tests for Apex language
+// https://github.com/is-nobody/apex-lang
+// MIT license
+
 #ifdef _WIN32
 __declspec(dllexport) long add(long a, long b) {
     return a + b;
