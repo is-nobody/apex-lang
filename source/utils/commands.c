a7a10db94a10f9d6ffc7d2534cbcdc2fae34b8e6
is-nobody <winstonsalam@proton.me>
2026-08-27 18:05:29 +0700

    refactor: remove msvc and clean up compiler version display

--- source/utils/commands.c
+++ source/utils/commands.c
@@ -24,21 +24,40 @@
 #endif
 
 #if defined(__clang__)
-#define COMPILER_INFO "Clang " __clang_version__
+#define COMPILER_NAME "Clang"
+#define COMPILER_VERSION_STRING __clang_version__
 #elif defined(__GNUC__)
-#define COMPILER_INFO "GCC " __VERSION__
-#elif defined(_MSC_VER)
-#define COMPILER_INFO "MSVC"
+#define COMPILER_NAME "GCC"
+#define COMPILER_VERSION_STRING __VERSION__
 #else
-#define COMPILER_INFO "Unknown Compiler"
+#define COMPILER_NAME "Unknown Compiler"
+#define COMPILER_VERSION_STRING ""
 #endif
+
+// extracts major.minor.patch from compiler version string
+static void get_compiler_version(char* buffer, size_t size) {
+    const char* ver = COMPILER_VERSION_STRING;      // get version string from compiler
+    size_t i = 0;                                   // index into output buffer
+    
+    while (*ver && !isdigit(*ver)) {                // find first digit in string
+        ver++;                                      // advance past non-digits
+    }
+    
+    while (*ver && *ver != ' ' && i < size - 1) {   // copy version until space
+        buffer[i++] = *ver++;                       // store char and advance
+    }
+    buffer[i] = '\0';                               // null terminate the string
+}
 
 // dispatches cli commands like 'version', 'build', and 'compile'
 int handle_commands(int argc, char** argv) {
     if (argc < 2) return -1;                                                    // need at least one argument
 
     if (strcmp(argv[1], "version") == 0) {                                      // version command
-        printf("Apex 26.08 [%s] on %s\n", COMPILER_INFO, platform_get_name());  // print version info
+        char compiler_ver[64] = {0};                                            // buffer for clean version
+        get_compiler_version(compiler_ver, sizeof(compiler_ver));               // extract major.minor.patch
+        
+        printf("Apex 26.08 [%s %s] on %s\n", COMPILER_NAME, compiler_ver, platform_get_name());  // print version info
         return 0;                                                               // success
     }
 
@@ -50,5 +69,5 @@ int handle_commands(int argc, char** argv) {
         return compile_command(argc, argv); // delegate to compile handler
     }
 
-    return -1;                             // unknown command
+    return -1;                              // unknown command
 }
